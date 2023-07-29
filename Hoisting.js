84f79d8e41f374ed36212bb56a3293fe9dbbda77
Juned Ahmad <junedahmad9296@gmail.com>
2023-07-29 10:45:15 +0530

    Add files via upload

---
+++ Hoisting.js
@@ -0,0 +1,18 @@
+
+      // Q- Explain the Hoisting in JavaScript?
+
+      // // Ans- Hoisting is the default behaviour of JavaScript
+      // where every varriable and Function() declariation moves
+      // on the top not initialization.
+
+      // For Example --
+
+        a = 1000;
+        console.log(a);
+        var a;
+
+      greet();
+      function greet() {
+        console.log("Good Morning");
+      }
+  
