b661e2f80c74c4a19bcab4f880f1a30626103a5d
Juned Ahmad <junedahmad9296@gmail.com>
2023-08-17 06:20:53 -0700

    Add files via upload

---
+++ P33-Function_declaration & expression.js
@@ -0,0 +1,17 @@
+// Function declaration -
+
+//  addNumbers takes two parameters and returns their sum
+function addNumbers(a, b) {
+  return a + b; // Return the sum of 'a' and 'b'
+}
+console.log(addNumbers(2,3));
+
+
+
+// Function expression -
+
+//  multiplyNumbers takes two parameters and returns their product
+const multiplyNumbers = function (a, b) {
+  return a * b; // Return the product of 'a' and 'b'
+};
+console.log(multiplyNumbers(2,3));
