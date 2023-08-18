75f03cb341541ce086756a07b734624d0a42fb83
Juned Ahmad <junedahmad9296@gmail.com>
2023-08-18 09:27:54 -0700

    Update P34-Diff_==_===Operator.js

--- P34-Diff_==_===Operator.js
+++ P34-Diff_==_===Operator.js
@@ -1,13 +1,8 @@
 /*
 Q. What is the Diffrence between in == & === in javascript ?
 
-Ans- In JavaScript, == and === are comparison operators used to compare values. Here's the simple difference:
-
-     == (Equality): This operator checks if two values are equal in terms of their content. It performs type coercion,
-     which means it tries to convert the values to the same type before comparison if they are of different types.
-
-     === (Strict Equality): This operator also checks if two values are equal, but it is more strict. It not only compares
-     their content but also their data types. Both the values and their types must be the same for the comparison to be true.
+Ans- both are comparison operator .the difference between both the operator is that "==" is used to compare values. 
+     where "===" is used to compare both the values and types.
     */
 
 5 == "5"; // true, because the values are equal after conversion
