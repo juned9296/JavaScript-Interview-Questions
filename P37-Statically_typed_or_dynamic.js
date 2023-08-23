524e88f5bf388e6c0179ac1e6009a1ac8b75a899
Juned Ahmad <junedahmad9296@gmail.com>
2023-08-23 00:19:04 -0700

    Add files via upload

---
+++ P37-Statically_typed_or_dynamic.js
@@ -0,0 +1,20 @@
+/*
+
+Q.  is JavaDcript Statically Typed or Dynamically Typed Language ?
+
+Ans- JavaScript is a dynamically typed language.because you don't need 
+     to specify the data type of a variable when you declare it. 
+     The type of a variable is determined at runtime based on the value
+     assigned to it. This allows for more flexible coding but can also 
+     lead to type-related errors if not handled carefully. */
+
+    let variable = 10; // variable is a number
+    console.log(variable); // Output: 10
+
+    variable = "Hello, world!"; // variable is now a string
+    console.log(variable); // Output: Hello, world!
+
+    // In this example, the variable variable starts as a number,
+    // but it's later assigned a string value. JavaScript allows this 
+    // change in data type without requiring explicit type declarations.
+    // This demonstrates the dynamic typing nature of JavaScript.
