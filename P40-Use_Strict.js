7d29d4d05adcd241bfb72f922c2745ed4c6f2626
Juned Ahmad <junedahmad9296@gmail.com>
2023-08-24 23:15:11 -0700

    Add files via upload

---
+++ P40-Use_Strict.js
@@ -0,0 +1,22 @@
+/*
+ Q. What is useStrict in JavaScript ?
+
+ Ans.use strict" in JavaScript is like a safety mode you can turn on
+     at the start of your code. It helps prevent common coding mistakes
+     and enforces stricter rules, making your code less likely to have 
+     hidden bugs or unexpected behavior. */
+     
+   "use strict";  
+     
+
+
+function addsomthing(X,Y){
+  let add = X+Y;
+  return add
+}
+console.log(addsomthing(10,5));
+
+
+
+
+
